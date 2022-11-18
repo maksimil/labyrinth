313c8e7e7bcb3987cf9d6ea7a283a7ee53dec442
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-11-18 18:34:06 +0300

    coloring VISITED==0

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ Labyrinth lab_create(size_t width, size_t height) {
 
   lab.connect = malloc(sizeof(*lab.connect) * width);
   for (uint16_t i = 0; i < width; i++) {
-    lab.connect[i] = malloc(sizeof(*lab.connect[i] * height));
+    lab.connect[i] = malloc(sizeof(*lab.connect[i]) * height);
     for (uint16_t j = 0; j < height; j++) {
       lab.connect[i][j] = ENABLE;
     }
@@ -55,36 +55,57 @@ const char *WALL = " ";
   else                                                                         \
     printf("%s", WALL);
 
+uint8_t lab_get(const Labyrinth *lab, int16_t x, int16_t y, uint8_t d) {
+  if (x >= 0 && x < lab->width && y >= 0 && y < lab->height) {
+    return lab->connect[x][y];
+  } else {
+    return d;
+  }
+}
+
 void lab_print(const Labyrinth *lab) {
   // first row
   printf("%s", WALL);
 
   for (uint16_t x = 0; x < lab->width; x++) {
-    DISPLAY(x, 0, UP);
-    printf("%s", WALL);
+    printf("%s%s", WALL, WALL);
   }
   printf("\n");
 
   for (uint16_t y = 0; y < lab->height; y++) {
-    DISPLAY(0, y, LEFT);
+    printf("%s", WALL);
     for (uint16_t x = 0; x < lab->width; x++) {
       if (lab->connect[x][y] & START)
         printf("\x1b[32m");
       else if (lab->connect[x][y] & FINISH)
         printf("\x1b[31m");
+      else if ((lab->connect[x][y] & VISITED) == 0)
+        printf("\x1b[33m");
 
       DISPLAY(x, y, ENABLE);
 
       printf("\x1b[0m");
 
+      if ((x < lab->width - 1) && (lab->connect[x][y] & VISITED) == 0 &&
+          (lab->connect[x + 1][y] & VISITED) == 0)
+        printf("\x1b[33m");
+
       DISPLAY(x, y, RIGHT);
+
+      printf("\x1b[0m");
     }
 
     printf("\n");
 
     printf("%s", WALL);
     for (uint16_t x = 0; x < lab->width; x++) {
+      if ((y < lab->height - 1) && ((lab->connect[x][y] & VISITED) == 0) &&
+          ((lab->connect[x][y + 1] & VISITED) == 0))
+        printf("\x1b[33m");
       DISPLAY(x, y, DOWN);
+
+      printf("\x1b[0m");
+
       printf("%s", WALL);
     }
 
@@ -147,6 +168,12 @@ int main() {
   Labyrinth lab = lab_create(width, height);
 
   lab_generate(&lab, 0, 0, width - 1, height - 1);
+
+  for (uint16_t i = 0; i < 10; i++) {
+    for (uint16_t j = 0; j < 10; j++) {
+      lab.connect[i][j] &= 255 ^ VISITED;
+    }
+  }
 
   lab_print(&lab);
 
